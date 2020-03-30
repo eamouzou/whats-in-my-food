7eefb54c40c0c6a8e04dbe7fce8848ed01050986
Elom Amouzou <elom1990@yahoo.com>
2020-03-30 10:53:20 -0600

    revise index action using environment variable, test passes

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,15 +4,14 @@ class FoodsController < ApplicationController
     ingredient = params[:q]
     ingredient_for_url = ingredient.split(' ').join('%20')
 
-    api_key = 'KFcwzS9t1s2t0vaLj4LJOctunLr03wXaAKEgmnbL'
-
     conn = Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
-      faraday.headers["FOOD-API-KEY"] = api_key
+      faraday.headers["FOOD-API-KEY"] = ENV["FOOD_API_KEY"]
       faraday.adapter Faraday.default_adapter
     end
 
-    response = conn.get("/fdc/v1/search?api_key=#{api_key}&ingredients=#{ingredient_for_url}")
+    response = conn.get("/fdc/v1/search?api_key=#{ENV["FOOD_API_KEY"]}&ingredients=#{ingredient_for_url}")
     json = JSON.parse(response.body, symbolize_names: true)
+    
     @full_response = json
     @foods = json[:foods].take(10)
   end
